38c732e1053fd6b51ab73309d5ea93338d651515
Samuel Grayson <sagrays@sandia.gov>
2022-06-29 11:25:39 -0600

    Fix precommit script in template

--- templates/pre-commit.sh
+++ templates/pre-commit.sh
@@ -4,7 +4,7 @@
 set -o errexit -o nounset -o xtrace -o pipefail
 
 rm --recursive --force examples
-nix --keep-going --show-trace --print-build-logs build .#examples
+nix --keep-going --show-trace --print-build-logs build
 cp --dereference --recursive result examples
 chmod --recursive u+rw,g+rw examples
 unlink result
